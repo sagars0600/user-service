16fdfc1ddec817f594fa631e4d8408e7f9befa57
senil <senila@maveric-systems.com>
2022-03-21 17:26:52 +0530

    Created Get all user feature

--- src/main/java/com/user/userservice/controller/UserController.java
+++ src/main/java/com/user/userservice/controller/UserController.java
@@ -1,19 +1,32 @@
 package com.user.userservice.controller;
 
 
+import com.user.userservice.model.User;
 import com.user.userservice.repo.UserRepository;
 import com.user.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
+
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+      List<User> list=  userService.getAllUser();
+      return  new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+
+
 
 
 }
